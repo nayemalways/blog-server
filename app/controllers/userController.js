006c6de1e424e113f97b9f72b27171ef0ed2fd46
mohammad-nayem <nishanahmed13913@gmail.com>
2025-01-07 19:25:00 +0600

    Set user's Login token in the cookie storage

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -10,6 +10,11 @@ import { UserLogin, UserRegistration } from "../../services/userServices.js";
  // User Login 
  export const Login = async (req, res) => {
    const result = await UserLogin(req);
+
+   // User AUTH token set to cookie
+   const cookieOptions = {expires: new Date(Date.now() + 24*60*60*1000), httpOnly: false};
+   res.cookie('token', result['Token'], cookieOptions);
+
    res.json(result);
  }
 
