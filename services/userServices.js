cf3eb9a13d54a52974b47cd904ea1e839c8ec30b
mohammad-nayem <nishanahmed13913@gmail.com>
2025-01-07 18:40:27 +0600

    hashing user's password for better security

--- services/userServices.js
+++ services/userServices.js
@@ -1,15 +1,34 @@
+ import bcrypt from 'bcrypt';
+
  import UserModel from "../app/model/userModel.js";
  import {EncodeToken} from "../app/utility/tokenUtility.js";
 
+
+
+ 
  export const UserRegistration = async (req) => {
    try {
-      const reqBody = req.body;
-      if(reqBody.email && reqBody.password && reqBody.firstName && reqBody.lastName && reqBody.phone && reqBody.img){
-        const data = await UserModel.create(reqBody);
-        return {status: "success", data: data}
-      }else{
-        return {status: "Uncomplete", messag: "Please input all data"};
-      }
+    const {email, password, firstName, lastName, phone, img} = req.body;
+
+    // Hasing user's password for better security
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    
+    // Ready of Registration
+    const userData = {
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        phone,
+        img
+    }
+
+
+    // Send data to DB for User Registrations
+    const data = await UserModel.create(userData);
+    return {status: "success", data: data}
+ 
    }catch(e){
       return {status: "Error", message: e.toString()}
    }
