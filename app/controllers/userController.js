f7506a2413b02e136cd0afdb3ce00726c78fdbfb
mohammad-nayem <nishanahmed13913@gmail.com>
2025-01-16 17:32:12 +0600

    path added in set Cookie inside the login controllers

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -19,7 +19,11 @@ import {
    const result = await UserLogin(req);
 
    // User AUTH token set to cookie
-   const cookieOptions = {expires: new Date(Date.now() + 24*60*60*1000), httpOnly: false};
+   const cookieOptions = {
+    expires: new Date(Date.now() + 30*24*60*60*1000), 
+    httpOnly: false,
+    path: "/"
+  };
    res.cookie('token', result['Token'], cookieOptions);
 
    res.json(result);
