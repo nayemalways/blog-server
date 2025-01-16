5c1438f34ee0bb7b9a11f577a4aa52a4b510c5e1
mohammad-nayem <nishanahmed13913@gmail.com>
2025-01-17 00:06:25 +0600

    cookie refactored

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -21,7 +21,7 @@ import {
    // User AUTH token set to cookie
    const cookieOptions = {
     expires: new Date(Date.now() + 30*24*60*60*1000), 
-    httpOnly: true,
+    httpOnly: false,
     sameSite: "none",
     secure: true,
     path: "/"
